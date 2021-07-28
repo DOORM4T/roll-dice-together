f13635c77c44860a4ef0cca059281268d723438b
Mat Seto <seanseto@gmail.com>
2021-07-27 21:39:43 -0700

    feat (App.tsx): Remove boilerplate styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Box, Heading } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import React from "react"
-import usePeerDiceRoller from "./components/PeerDiceRoller/usePeerDiceRoller"
 import PeerContainer from "./components/PeerContainer"
 import useConnectionHandlers from "./components/PeerContainer/useConnectionHandlers"
+import usePeerDiceRoller from "./components/PeerDiceRoller/usePeerDiceRoller"
 
 const App = () => {
   const { onConnectionOpen, onConnectionClose } = useConnectionHandlers()
@@ -12,11 +12,12 @@ const App = () => {
     <Box
       width="100vw"
       minHeight="100vh"
-      bg="linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);"
-      color="white"
+      overflowX="hidden"
+      overflowY="auto"
+      bg="orange.200"
       textAlign="center"
+      pt="1rem"
     >
-      <Heading pt="1rem">PeerJS-React Template</Heading>
       <PeerContainer
         render={render}
         onPeerAction={onPeerAction}
